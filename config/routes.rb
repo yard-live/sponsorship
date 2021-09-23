b64f7c2e9f9c3e1fb6d68d0bc3678daddf76fff3
Miguel Tineo <miketineo@gmail.com>
2021-09-23 21:59:58 +0200

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sponsorships 
+  resources :webhooks, only: [:create]
 
   root 'static_pages#root'
 end
